9379d8a1c4b8d118e0d96c59e48c0e8d4c69da89
Dean Clarke <dclarke@splunk.com>
2022-10-31 19:33:35 -0700

    update per testing

--- bin/putApp.py
+++ bin/putApp.py
@@ -520,6 +520,7 @@ try:
                     params += "&defaultFeatures=false"
                 
                 if args.zookeeper is not None:
+                    print(f"I AM HERERERERE ==================== {args}")
                     if 'properties' in payload:
                         if 'zk_host_string' in payload['properties']:
                             payload['properties']['zk_host_string'] = args.zookeeper
